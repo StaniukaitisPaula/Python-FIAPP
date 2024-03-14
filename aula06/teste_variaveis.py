f1f263a807e040038159710a27a37edc9efea248
StaniukaitisPaula <paulablesa6@gmail.com>
2024-03-14 12:12:49 -0300

    aula 06

--- aula06/teste_variaveis.py
+++ aula06/teste_variaveis.py
@@ -1,15 +1,10 @@
 a = 10 # int
-
-b = False # boolean
-
-c = "Hello World" # string 
-
-d = 17.84 # float
-
-cp1 = 8.5 # float
-cp4 = 7.6 # float
-cp3 = 0.0 # float
-
+b = False  # boolean
+c = "Hello World"  # string
+d = 17.84  # float
+cp1 = 8.5  # float
+cp2 = 7.6  # float
+cp3 = 0.0  # float 
 #             0    1    2
 cp_lista = [ 8.5, 7.6, 0.0 ]     # lista
 print("Segundo valor da lista: ", cp_lista[1] )
---
+++ aula06/teste_variaveis1.py
@@ -0,0 +1,4 @@
+a = 10 # int
+b = False  # boolean
+c = "Hello World"  # string
+d = 17.84  # float
